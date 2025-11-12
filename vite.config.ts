9e7a12b1802b693e3a64f0fffc66b6c918e0666a
MM-HO <sk5388654@gmail.com>
2025-11-12 17:20:19 +0500

    build updated

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
-export default defineConfig(({ mode }) => {
-  return {
-    base: mode === 'production' ? '/Hybrid-POS/' : '/', // use / for dev, /POS/ for prod
-    plugins: [vue()],
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
-      }
+export default defineConfig({
+  base: '/Hybrid-POS/', // repo name with trailing slash
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
